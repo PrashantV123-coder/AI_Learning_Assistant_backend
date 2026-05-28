3681757b4d20fa36898b7d4218c6240d08b2e43b
Prashant Vishwakarma <prashantvishwakarma1516@gmail.com>
2026-05-28 16:47:17 +0530

    cors update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 //Middleware to handle cors
 app.use(
     cors({
-        origin: "*",
+        origin: "https://ai-learning-assistant-frontend-liard.vercel.app/",
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
     })
